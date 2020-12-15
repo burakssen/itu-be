62728945a1dd16f59bed234270bf6a1e3eb05075
burak ŞEN <buraksen7@hotmail.com>
2020-12-15 22:31:55 +0300

    Some bugs solved

--- entities/Class.py
+++
@@ -1,8 +0,0 @@
-class Class():
-    def __init__(self,className,classCode,tutor,videoList,department,review_point):
-        self.className = className
-        self.classCode = classCode
-        self.tutor = tutor
-        self.videoList = videoList
-        self.department = department
-        self.review_point = review_point
